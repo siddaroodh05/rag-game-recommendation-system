cbc1fea7bb0dcfd7454bd4c6d69158f509276a31
SIDDAROODH <siddaroodh2004@gmail.com>
2026-09-23 17:12:18 +0530

    added resource config

---
+++ Backend/src/test/java/com/siddu/gamesense/GameSenseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siddu.gamesense;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GameSenseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
